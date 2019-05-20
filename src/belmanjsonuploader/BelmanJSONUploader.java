73826b69e322c38558f77b857946386b0dd3dc77
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-05-20 09:45:30 +0200

    Executor

--- src/belmanjsonuploader/BelmanJSONUploader.java
+++ src/belmanjsonuploader/BelmanJSONUploader.java
@@ -15,20 +15,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
+import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
- *
+ * This project is for reading JSON files and uploading the information to the
+ * database for the Belman project
  * @author Theis
  */
 public class BelmanJSONUploader
 {
 
     private DbConnectionProvider ds;
-
+    
     public BelmanJSONUploader() throws IOException
     {
         ds = new DbConnectionProvider();
@@ -41,108 +48,104 @@ public class BelmanJSONUploader
      */
     public static void main(String[] args) throws FileNotFoundException, SQLException, IOException
     {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
         BelmanJSONUploader jsonupload = new BelmanJSONUploader();
-
-        try
+        
+        Runnable task = () ->
         {
-            jsonupload.checkJSONFolder(new File("JSON"));
-        } catch (IOException ex)
-        {
-            ex.printStackTrace();
-        } catch (ParseException ex)
-        {
-            ex.printStackTrace();
-        }
+            try
+            {
+                jsonupload.checkJSONFolder(new File("JSON"));
+            } catch (IOException | ParseException | SQLException ex)
+            {
+                Logger.getLogger(BelmanJSONUploader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        };
+        
+        executor.scheduleWithFixedDelay(task, 0, 30, TimeUnit.SECONDS);
     }
-
+    
     public void uploadJSON(String path) throws FileNotFoundException, IOException, ParseException, SQLException
     {
         Object obj = new JSONParser().parse(new FileReader(path));
         
         JSONObject jObject = (JSONObject) obj;
-
-            try (Connection con = ds.getConnection())
-            {
-        int id = 0;
-        JSONArray jArray = (JSONArray) jObject.get("ProductionOrders");
-        for (Object object : jArray) //Iterates over an array of production orders
+        
+        try (Connection con = ds.getConnection())
         {
-            JSONObject oObject = (JSONObject) object;
-
-            JSONObject pOObject = (JSONObject) oObject.get("Customer");
-            String customerName = (String) pOObject.get("Name");
-
-            JSONObject orderObject = (JSONObject) oObject.get("Order");
-            String orderNumber = (String) orderObject.get("OrderNumber");
-
-            JSONObject deliveryObject = (JSONObject) oObject.get("Delivery");
-            String deliveryDateString = (String) deliveryObject.get("DeliveryTime");
-            Date deliveryDate = formatDateString(deliveryDateString);
-
+            int id = 0;
+            JSONArray jArray = (JSONArray) jObject.get("ProductionOrders");
+            for (Object object : jArray) //Iterates over an array of production orders
+            {
+                JSONObject oObject = (JSONObject) object;
+                
+                JSONObject pOObject = (JSONObject) oObject.get("Customer");
+                String customerName = (String) pOObject.get("Name");
+                
+                JSONObject orderObject = (JSONObject) oObject.get("Order");
+                String orderNumber = (String) orderObject.get("OrderNumber");
+                
+                JSONObject deliveryObject = (JSONObject) oObject.get("Delivery");
+                String deliveryDateString = (String) deliveryObject.get("DeliveryTime");
+                Date deliveryDate = formatDateString(deliveryDateString);
+                
                 if (!checkIfOrderExists(con, orderNumber))
                 {
-                id = uploadPOrderToDatabase(orderNumber, customerName, deliveryDate, con);
+                    id = uploadPOrderToDatabase(orderNumber, customerName, deliveryDate, con);
                 }
-            
-            
-            JSONArray dTaskArray = (JSONArray) oObject.get("DepartmentTasks");
-            for (Object object1 : dTaskArray) //Iterates over an array of departmentTasks within each production order
-            {
-                JSONObject dTaskObject = (JSONObject) object1;
-
-                JSONObject dObject = (JSONObject) dTaskObject.get("Department");
-                String departmentName = (String) dObject.get("Name");
-
-                String endDateString = (String) dTaskObject.get("EndDate");
-                Date endDate = formatDateString(endDateString);
-
-                String startDateString = (String) dTaskObject.get("StartDate");
-                Date startDate = formatDateString(startDateString);
-
-                boolean finishedTask = (boolean) dTaskObject.get("FinishedOrder");
-
-//                try (Connection con = ds.getConnection())
-//                {
+                
+                JSONArray dTaskArray = (JSONArray) oObject.get("DepartmentTasks");
+                for (Object object1 : dTaskArray) //Iterates over an array of departmentTasks within each production order
+                {
+                    JSONObject dTaskObject = (JSONObject) object1;
+                    
+                    JSONObject dObject = (JSONObject) dTaskObject.get("Department");
+                    String departmentName = (String) dObject.get("Name");
+                    
+                    String endDateString = (String) dTaskObject.get("EndDate");
+                    Date endDate = formatDateString(endDateString);
+                    
+                    String startDateString = (String) dTaskObject.get("StartDate");
+                    Date startDate = formatDateString(startDateString);
+                    
+                    boolean finishedTask = (boolean) dTaskObject.get("FinishedOrder");
+                    
                     if (id > 0 && !checkIfTaskExists(con, departmentName, id))
                     {
-                    uploadDTaskToDatabase(departmentName, startDate, endDate, finishedTask, 0, con, id);
+                        uploadDTaskToDatabase(departmentName, startDate, endDate, finishedTask, 0, con, id);
                     }
-
-//                } catch (Exception e)
-                {
+                    
                 }
-
+                
             }
-
-        }
         }
     }
-
+    
     public int uploadPOrderToDatabase(String orderNumber, String customerName, Date deliveryDate, Connection con) throws SQLException
     {
         PreparedStatement pstmt = con.prepareStatement("INSERT INTO ProductionOrder VALUES (?,?,?)", Statement.RETURN_GENERATED_KEYS);
-
+        
         java.sql.Date sqlDate = new java.sql.Date(deliveryDate.getTime());
         pstmt.setString(1, orderNumber);
         pstmt.setString(2, customerName);
         pstmt.setDate(3, sqlDate);
-
+        
         pstmt.execute();
         
         ResultSet rs = pstmt.getGeneratedKeys();
         int id = 0;
         if (rs.next())
         {
-             id = rs.getInt(1);
+            id = rs.getInt(1);
         }
         return id;
     }
-
+    
     public void uploadDTaskToDatabase(String departmentName, Date startDate, Date endDate, boolean finishedTask, int timeOffset, Connection con, int id) throws SQLException
     {
-
+        
         PreparedStatement pstmt1 = con.prepareStatement("INSERT INTO DepartmentTask VALUES (?,?,?,?,?,?)");
-
+        
         java.sql.Date sqlStartDate = new java.sql.Date(startDate.getTime());
         java.sql.Date sqlEndDate = new java.sql.Date(endDate.getTime());
         
@@ -152,10 +155,10 @@ public class BelmanJSONUploader
         pstmt1.setDate(4, sqlEndDate);
         pstmt1.setInt(5, timeOffset);
         pstmt1.setBoolean(6, finishedTask);
-
+        
         pstmt1.execute();
     }
-
+    
     private Date formatDateString(String dateString)
     {
         Long milli = Long.parseLong(dateString.substring(6, dateString.indexOf("+")));
